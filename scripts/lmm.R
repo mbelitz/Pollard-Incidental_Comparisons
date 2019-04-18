7692a6c57aa01164767aae2c9c8cc3fc78bb4f4b
Michael Belitz <michaelbelitz06@gmail.com>
2019-04-18 18:43:29 -0400

    lmm update

--- scripts/lmm.R
+++ scripts/lmm.R
@@ -1,10 +1,6 @@
-# first run incidental phenology estimates onset script
-
-# Then run this script
-
 library(MuMIn)
 
-pt_onset <- total_onset_centroid %>% 
+pt_onset <- total_est_centroid_8spp %>% 
   filter(SciName == "Phyciodes tharos") %>% 
   select(polyids, SciName, year, onset, high_ci, low_ci, long_centroid.x, lat_centroid.x)
 
@@ -18,4 +14,32 @@ summary(pt_lmm)
 
 pt_lmm_2018 <- lm(onset ~ lat_centroid.x, data = pt_onset_2018)
 summary(pt_lmm_2018)
+
+
+
+
+# LMM for Epargyreus clarus
+ec_onset <- total_est_centroid_8spp %>% 
+  filter(SciName == "Epargyreus clarus") %>% 
+  select(polyids, SciName, year, onset, high_ci, low_ci, long_centroid.x, lat_centroid.x)
+
+ec_lmm <- lm(onset ~ lat_centroid.x + factor(year), data = ec_onset)
+summary(ec_lmm)
+
+
+
+# LMM for Pieris rapae
+pr_onset <- total_est_centroid_8spp %>% 
+  filter(SciName == "Pieris rapae") %>% 
+  select(polyids, SciName, year, onset, high_ci, low_ci, long_centroid.x, lat_centroid.x)
+
+pr_onset_2018 <- pr_onset %>% 
+  filter(year == 2018) %>% 
+  filter(onset > 0)
+
+pr_lmm <- lm(onset~ lat_centroid.x + factor(year), data = pr_onset)
+summary(pr_lmm)
+
+pr_lmm_2018 <- lm(onset~ lat_centroid.x, data = pr_onset_2018)
+summary(pr_lmm)
 
