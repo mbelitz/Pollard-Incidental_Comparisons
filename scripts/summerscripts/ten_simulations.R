6ec9c0d61801a49be109e22596b70f386d6e5e5d
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-11 13:32:46 -0400

    tenth percentile simulations

---
+++ scripts/summerscripts/ten_simulations.R
@@ -0,0 +1,65 @@
+library(ggplot2)
+library(dplyr)
+
+ten.sim <- read.csv(file = "outputs/total_sims.csv", stringsAsFactors = FALSE,
+                      h = TRUE)
+
+ten.sim$sd <- as.factor(ten.sim$sd)
+ten.sim$obs <- as.factor(ten.sim$obs)
+
+
+ten.sim.summary <- ten.sim %>% 
+  group_by(obs, sd) %>% 
+  summarize(count = n(), meanten = mean(estimate), meandis = mean(distance),
+            meanci = mean(ci), meanhighci = mean(highci), meanlowci = mean(lowci))
+
+
+ten.sim.summary.pass <- ten.sim %>% 
+  group_by(obs, sd, pass) %>% 
+  summarize(count = n(), meanten = mean(estimate), meandis = mean(distance),
+            meanci = mean(ci), meanhighci = mean(highci), meanlowci = mean(lowci))
+
+# 10 obs
+
+ten.sim.summary.10 <- ten.sim.summary %>% 
+  filter(obs == "10") %>% 
+  mutate(uid = paste(obs, sd))
+
+ggplot(ten.sim.summary.10, aes(x = uid, y = meanten)) + 
+  geom_bar(stat = "identity", aes(fill = sd), alpha = 0.65) +
+  scale_fill_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") + 
+  geom_errorbar(aes(ymin = meanlowci, ymax = meanhighci)) +
+  theme_classic()
+
+# 20 obs
+
+ten.sim.summary.20 <- ten.sim.summary %>% 
+  filter(obs == "20") %>% 
+  mutate(uid = paste(obs, sd))
+
+ggplot(ten.sim.summary.20, aes(x = uid, y = meanten)) + 
+  geom_bar(stat = "identity", aes(fill = sd), alpha = 0.65) +
+  scale_fill_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") + 
+  geom_errorbar(aes(ymin = meanlowci, ymax = meanhighci)) +
+  theme_classic()
+
+# 50 obs
+
+ten.sim.summary.50 <- ten.sim.summary %>% 
+  filter(obs == "50") %>% 
+  mutate(uid = paste(obs, sd))
+
+ggplot(ten.sim.summary.50, aes(x = uid, y = meanten)) + 
+  geom_bar(stat = "identity", aes(fill = sd), alpha = 0.65) +
+  scale_fill_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") + 
+  geom_errorbar(aes(ymin = meanlowci, ymax = meanhighci)) +
+  theme_classic()
