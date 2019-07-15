4d7d459a883a12ddd6b8140316ad76904495107c
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-15 13:53:10 -0400

    sperated simulation figures into threes

--- scripts/summerscripts/simulation_vis.R
+++ scripts/summerscripts/simulation_vis.R
@@ -7,6 +7,58 @@ total_sims$obs <- as.factor(total_sims$obs)
 
 str(total_sims)
 
+# 10 obs
+
+ten.sim.summary.10 <- total_sims %>% 
+  filter(obs == "10") %>% 
+  mutate(uid = paste(obs, sd))
+
+bp10per_10obs <- ggplot(ten.sim.summary.10) +
+  geom_boxplot(aes(x = uid, y = estimate, color = sd)) + 
+  scale_color_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") +
+  scale_y_continuous(expand = c(0,0)) +
+  ggtitle(label = "10 Observations", subtitle = "Lines are True 10%, Points are Outliers") +
+  labs(x = "Observations SD", y = "10% Estimates of 30 Trials") +
+  theme_classic()
+
+# 20 obs
+
+ten.sim.summary.20 <- total_sims %>% 
+  filter(obs == "20") %>% 
+  mutate(uid = paste(obs, sd))
+
+bp10per_20obs <- ggplot(ten.sim.summary.20) +
+  geom_boxplot(aes(x = uid, y = estimate, color = sd)) + 
+  scale_color_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") +
+  scale_y_continuous(expand = c(0,0)) +
+  ggtitle(label = "20 Observations", subtitle = "Lines are True 10%, Points are Outliers") +
+  labs(x = "Observations SD", y = "10% Estimates of 30 Trials") +
+  theme_classic()
+
+# 50 obs
+
+ten.sim.summary.50 <- total_sims %>% 
+  filter(obs == "50") %>% 
+  mutate(uid = paste(obs, sd))
+
+bp10per_50obs <- ggplot(ten.sim.summary.50) +
+  geom_boxplot(aes(x = uid, y = estimate, color = sd)) + 
+  scale_color_manual(values = c("red", "cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(137.17)), color = "red") +
+  geom_hline(aes(yintercept = c(124.8)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(99.24)), color = "blue") +
+  scale_y_continuous(expand = c(0,0)) +
+  ggtitle(label = "50 Observations", subtitle = "Lines are True 10%, Points are Outliers") +
+  labs(x = "Observations SD", y = "10% Estimates of 30 Trials") +
+  theme_classic()
+
+
 ggplot(total_sims) + 
   geom_boxplot(aes(x = ob_sd, y = estimate, fill = obs, color = sd)) +
   scale_color_manual(values = c("black", "grey", "navy")) +
@@ -25,8 +77,6 @@ summary_sims <- total_sims %>%
   summarise(pass = sum(pass_num), mean_dis = mean(distance), mean_ci = mean(ci))
 
 summary_sims$pass[9] <-  17
-
-ggplot(df2, aes(x=dose, y=len, fill=supp))
 
 ggplot(summary_sims, aes(x = ob_sd, y = abs(mean_dis))) +
   geom_bar(stat = "identity") +
