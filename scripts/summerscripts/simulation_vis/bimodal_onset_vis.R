792c7daa7515cf4cdea3e3d5355a53ee7d429e72
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-19 10:58:20 -0400

    first commit

---
+++ scripts/summerscripts/simulation_vis/bimodal_onset_vis.R
@@ -0,0 +1,76 @@
+library(dplyr)
+library(ggplot2)
+
+bm_on <- read.csv("simulation_outputs/bimodal_onset_sims.csv", stringsAsFactors = FALSE, header = TRUE)
+
+bm_on$sd <- as.factor(bm_on$sd)
+bm_on$obs <- as.factor(bm_on$obs)
+
+bm_on.summary <- bm_on %>% 
+  group_by(obs, sd, estimator) %>% 
+  summarize(count = n(), meanonset = mean(estimate), meandis = mean(distance),
+            meanci = mean(ci), meanhighci = mean(highci), meanlowci = mean(lowci))
+
+bm_on.summary.pass <- bm_on %>% 
+  group_by(obs, sd, estimator, pass) %>% 
+  summarize(count = n(), meanonset = mean(estimate), meandis = mean(distance),
+            meanci = mean(ci), meanhighci = mean(highci), meanlowci = mean(lowci))
+
+# 10 obs
+
+bm_on.summary.10 <- bm_on.summary %>% 
+  filter(obs == "10") %>% 
+  mutate(uid = paste(estimator, obs, sd))
+
+ggplot(bm_on.summary.10, aes(x = uid, y = meanonset)) + 
+  geom_bar(stat = "identity", aes(fill = sd), alpha = 0.65) +
+  scale_fill_manual(values = c("cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(113.29)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(76.58)), color = "blue") + 
+  geom_errorbar(aes(ymin = meanlowci, ymax = meanhighci)) +
+  theme_classic()
+
+bm_on.10 <- bm_on %>% 
+  filter(obs == "10") %>% 
+  mutate(uid = paste(estimator, obs, sd))
+
+bmonset_10obs <- ggplot(bm_on.10) +
+  geom_boxplot(aes(x = uid, y = estimate, color = sd, fill = estimator)) + 
+  scale_fill_manual(values = c("light grey", "white")) +
+  scale_color_manual(values = c("cyan3", "navy")) +
+  geom_hline(aes(yintercept = c(113.29)), color = "cyan3") +
+  geom_hline(aes(yintercept = c(76.58)), color = "blue") + 
+  scale_y_continuous(expand = c(0,0)) +
+  ggtitle(label = "10 Observations", subtitle = "Lines are True Onset, Points are Outliers") +
+  labs(x = "Observations SD", y = "Onset Estimates of 30 Trials") +
+  theme_classic()
+
+# pass summary
+
+bm_onset_pass_sum <- bm_on %>% 
+  mutate(pass_num = ifelse(pass == TRUE, 1,0)) %>% 
+  group_by(estimator, sd, obs) %>% 
+  summarise(pass = sum(pass_num), mean_dis = mean(distance), mean_ci = mean(ci))
+
+bm_onset_pass_sum <- bm_onset_pass_sum %>% 
+  mutate(uid = paste(estimator, sd, obs)) %>% 
+  mutate(percent_right = pass / 30)
+
+ggplot(bm_onset_pass_sum, aes(x = uid, y = abs(mean_dis))) +
+  geom_bar(stat = "identity", aes(fill = estimator)) +
+  geom_errorbar(aes(ymin = abs(mean_dis) - mean_ci/2, ymax = abs(mean_dis) + mean_ci/2)) +
+  labs(x = "SD - Observations", y = "Days from True Onset") + 
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+ggplot(bm_onset_pass_sum, aes(x = uid, y = percent_right)) +
+  geom_bar(stat = "identity", aes(fill = estimator)) +
+  labs(x = "SD - Observations", y = "Percent of Estimates") + 
+  ggtitle("Does the true onset fall within the CIs?") + 
+  ylim(min = 0, max = 1) +
+  scale_y_continuous(expand = c(0,0)) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+
+            
